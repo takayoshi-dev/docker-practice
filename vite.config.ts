fd4629f4ebe8bd35d4832bda5bc5fda1b47b7492
T.Tominaga <t.tominaga1130@gmail.com>
2025-12-27 04:19:18 +0000

    vitestを導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/docker-practice/",
   server: {
     port: 5173,
     host: "127.0.0.1",
+  },
+  test: {
+    include: ["tests/**/*.test.ts"],
+    environment: "jsdom",
+    globals: true,
   },
 });
